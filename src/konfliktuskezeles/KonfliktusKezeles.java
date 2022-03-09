157727bc9f4415fd568970bae8b9da49b36a49a2
Richard-0106 <78822662+Richard-0106@users.noreply.github.com>
2022-03-09 11:54:25 +0100

    3.név deklarálása

--- src/konfliktuskezeles/KonfliktusKezeles.java
+++ src/konfliktuskezeles/KonfliktusKezeles.java
@@ -7,8 +7,10 @@ public class KonfliktusKezeles {
   
     public static void main(String[] args) {
 
+      int i =7;
      String nev1="Alex";
      String nev2="Ricsi";
+      int nev3;
 
     }
     
